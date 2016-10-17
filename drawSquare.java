59834db658e4d0bd36bee060038d2c877c357624
201311239 이헌성 /201311239 <gene9329@gmail.com>
2016-10-17 16:32:56 +0900

    Update drawSquare.java

--- drawSquare.java
+++ drawSquare.java
@@ -1,10 +1,12 @@
-;
+/classpath lib/aplu5.jar
+import ch.aplu.turtle.*;
+
 void drawSquare(int size){
   for(int i=0;i<4;i++){
     t1.forward(size);
     t1.right(90);
   }
 }
-import ch.aplu.turtle.*;
+
 Turtle t1=new Turtle();
 drawSquare(100);
