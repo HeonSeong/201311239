dd093f323b80b6e3a84930c0a9fab093cae650ea
201311239 이헌성 /201311239 <gene9329@gmail.com>
2016-10-17 12:24:20 +0900

    Update TurtleDemoMain.java

--- TurtleDemoMain.java
+++ TurtleDemoMain.java
@@ -1,16 +1,21 @@
 package com.sd.turtle;
 import ch.aplu.turtle.*;
 
-class TurtleDemoMain{
- TurtleDemoMain(){
-  Turtle t1= new Turtle();
-  for(int i=0; i<4; i++){
-    t1.forward(50);
-    t1.left(50);
-    }
-	
-}
-public static void main(String[]args){
-  new TurtleDemoMain();
-}
+class TurtleDemoMain {
+  Turtle t1 = new Turtle();
+
+  TurtleDemoMain() {
+    t1.forward(100);
+    t1.right(90);
+    t1.forward(100);
+    t1.right(90);
+    t1.forward(100);
+    t1.right(90);
+    t1.forward(100);
+    t1.right(90);
+  }
+
+  public static void main(String[] args) {
+    new TurtleDemoMain();
+  }
 }
